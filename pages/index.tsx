bffcd3c7a051c12a0f6f9e6405528520c2807a62
r3nanp <renanmol87@gmail.com>
2020-09-11 16:29:58 -0300

    feat: add input

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,57 @@
-// import Head from 'next/head'
+import React, { FormEvent, useState } from 'react'
+import { Flex, Text, Button } from '@chakra-ui/core'
+import Input from '../components/Input'
 
 export default function Home() {
+  const [email, setEmail] = useState('')
+
+  function handleSignUpToNewsletter() {
+
+  }
+
   return (
-    <h1>Hello world!</h1>
+    <Flex as="main" height="100vh" justifyContent="center" alignItems="center">
+      <Flex
+        as="form"
+        onSubmit={handleSignUpToNewsletter}
+        backgroundColor="gray.700"
+        borderRadius="md"
+        flexDir="column"
+        alignItems="stretch"
+        padding={8}
+        marginTop={4}
+        width="100%"
+        maxW="400px"
+      >
+
+        <Text
+          textAlign="center"
+          fontSize="sm"
+          color="gray.400"
+          marginBottom={2}
+        >
+          Assine a newsletter do Renan e receba os melhores conteúdos sobre
+          programação e por incrível que pareça, música também!
+        </Text>
+
+        <Input
+          placeholder="Seu melhor e-mail"
+          marginTop={2}
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
+
+        <Button
+          type="submit"
+          backgroundColor="purple.500"
+          height="50px"
+          borderRadius="sm"
+          marginTop={6}
+          _hover={{ backgroundColor: 'purple.600' }}
+        >
+          INSCREVER
+        </Button>
+      </Flex>
+    </Flex>
   )
 }
